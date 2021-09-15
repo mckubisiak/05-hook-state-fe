a18791b147215ee2cd0a28d2f988209606883a5a
Kubi Kubisiak <mckubisiak@gmail.com>
2021-09-14 17:08:14 -0700

    finished testing with mock server set up, failing test now passing!

--- src/containers/SimpsonsQuote.test.jsx
+++ src/containers/SimpsonsQuote.test.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, waitFor, render, screen } from '@testing-library/react';
 import SimpsonsQuote from '../containers/SimpsonsQuote';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+
+const mockServer = setupServer(
+  rest.get('https://thesimpsonsquoteapi.glitch.me/quotes', (req, res, ctx) => {
+    return res(
+      ctx.json([
+        {
+          quote: 'howdy parnar',
+          character: 'Kubi-doo',
+          image: 'example.com/image.png',
+        },
+      ])
+    );
+  })
+);
 
 describe('simpson quote container', () => {
+  beforeAll(() => mockServer.listen());
+  afterAll(() => mockServer.close());
+
   it('button that displays random quote on click', async () => {
     render(<SimpsonsQuote />);
 
     const fetchButton = screen.getByRole('button', { name: 'fetch quote' });
+
+    fireEvent.click(fetchButton);
+
+    return waitFor(() => {
+      screen.getByText('howdy parnar');
+    });
   });
 });
